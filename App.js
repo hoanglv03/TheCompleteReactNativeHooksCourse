b7090bf08e78a4f980f6a49cefbadd249190f23c
hoanglv03 <luonghoang29022003@gmail.com>
2023-07-18 09:47:55 +0700

    add color

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
+import ComponentsScreen from "./src/screens/componentsScreen";
+import ListScreen from "./src/screens/ListScreen";
+import ImageScreen from "./src/screens/ImageScreen";
+import CounterScreen from "./src/screens/CounterScreen";
+import ColorScreen from "./src/screens/ColorScreen";
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
+    ComponentScreen: ComponentsScreen,
+    ListScreen: ListScreen,
+    ImageScreen: ImageScreen,
+    CounterScreen: CounterScreen,
+    ColorScreen: ColorScreen
   },
   {
     initialRouteName: "Home",
---
+++ src/screens/ColorScreen.js
@@ -0,0 +1,12 @@
+import { Button, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function ColorScreen() {
+  return (
+    <View>
+      <Button  title='add Color'/>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
